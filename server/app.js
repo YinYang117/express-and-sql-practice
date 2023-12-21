e0c84a332e599333bbdb5a1e60292ff748cac3e0
Ryan Schneider <slcryan2010@yahoo.com>
2023-12-21 12:35:34 -0700

    json and reqbody setup

--- server/app.js
+++ server/app.js
@@ -23,8 +23,17 @@ const {
 
 const express = require('express');
 const app = express();
+app.use(express.json()); // Allows the app to deserialize bodies with json content
 
-// Your code here 
+
+// for clarity of what's being passed in the req.body
+app.use((req, res, next) => {
+  console.log('Request Body:', req.body);
+  next();
+});
+
+
+
 
 // DO NOT MODIFY
 if (require.main === module) {
