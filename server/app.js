1936cec26c8206f30afce014a10fbca114cad954
Ryan Schneider <slcryan2010@yahoo.com>
2023-12-23 11:44:22 -0700

    finished a few routes up to 8

--- server/app.js
+++ server/app.js
@@ -1,4 +1,5 @@
 // Phase 2
+const e = require('express');
 const {
   getAllArtists,
   getLatestArtist,
@@ -33,11 +34,60 @@ app.use((req, res, next) => {
 });
 
 app.get('/artists', (req, res, next) => {
-  res.statusCode = 200;
+  res.status(200);
   res.header = {
     'Content-Type': 'application/json'
   }
   res.send(getAllArtists());
+})
+
+app.post('/artists', (req, res, next) => {
+  res.status(201);
+  res.setHeader("Content-Type", "application/json");
+  res.send(addArtist(req.body))
+})
+
+app.get('/artists/latest', (req, res, next) => {
+  res.setHeader("Content-Type", "application/json");
+  res.send(getLatestArtist());
+})
+
+app.get('/artists/latest/albums', (req, res, next) => {
+  res.setHeader("Content-Type", "application/json");
+  res.send(getAlbumsForLatestArtist());
+})
+
+app.get('/artists/:artistId', (req, res, next) => {
+  let id = req.params.artistId
+  res.setHeader("Content-Type", 'application/json');
+  res.send(getArtistByArtistId(id))
+})
+
+app.route('/artists/:artistId')
+  .put((req, res, next) => {
+    let id = req.params.artistId;
+    res.setHeader("Content-Type", 'application/json');
+    let updatedArtist = editArtistByArtistId(id, req.body);
+    res.send(updatedArtist);
+  })
+  .patch((req, res, next) => {
+  let id = req.params.artistId;
+  res.setHeader("Content-Type", 'application/json');
+  let updatedArtist = editArtistByArtistId(id, req.body);
+  res.send(updatedArtist);
+})
+
+app.delete('/artists/:artistId', (req, res, next) => {
+  let id = req.params.artistId;
+  deleteArtistByArtistId(id);
+  res.setHeader("Content-Type", 'application/json');
+  res.send({"message": "Successfully deleted"});
+})
+
+app.get('/artists/:artistId/albums', (req, res, next) => {
+  let id = req.params.artistId;
+  res.setHeader("Content-Type", 'application/json');
+  res.send(getAlbumsByArtistId(id));
 })
 
 
